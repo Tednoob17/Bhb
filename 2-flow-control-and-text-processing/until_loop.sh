31973174eb988d1e9c378b8450cc4e253ad1aee6
Ted Kouhouenou <ted.kouhouenou@epitech.eu>
2024-12-19 03:20:35 +0100

    random file and loop

---
+++ 2-flow-control-and-text-processing/until_loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE="output.txt"
+
+touch "${FILE}"
+until [[ -s "${FILE}"  ]]; do
+    echo "${FILE} is empty..."
+    echo "Checking again in 2 secondes..."
+    sleep 2
+done
+
+echo "${FILE} appears to have some content in it!"
